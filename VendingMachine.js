8ea157dd6d23097d00403fbc37c6ab09838bd3a3
pollyj <55009539+pollyj@users.noreply.github.com>
2020-04-07 17:50:24 +0900

    Initial commit; wrote insertCoin() & selectRow()
    Co-authored-by: rogerrogerv <60841571+rogerrogerv@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,59 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+/* exported stringifyJSON */
+
+class VendingMachine {
+  constructor() {
+    this.till = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+    this.balance = 0;
+    this.row = null;
+    this.column = null;
+    this.inventory = [[juice, coffee]];
+  }
+
+  insertCoin(denomination) {
+    this.till[denomination] += 1;
+    this.balance += denomination;
+  }
+
+  changeReturn() {}
+
+  selectRow(selectedRow) {
+    if (
+      selectedRow === "A" ||
+      selectedRow === "B" ||
+      selectedRow === "C" ||
+      selectedRow === "D"
+    ) {
+      this.row = selectedRow;
+    } else {
+      return "invalid row letter";
+    }
+  }
+
+  pressButton(selectedRow, selectedColumn) {
+    if (this.row !== null) {
+      this.selectRow(selectedRow);
+    }
+
+    if (
+      selectedColumn === 1 ||
+      selectedColumn === 2 ||
+      selectedColumn === 3 ||
+      selectedColumn === 4
+    ) {
+      this.column = selectedColumn;
+    } else {
+      return "invalid column number";
+    }
+  }
+}
+
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
+//test
 const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
+
+//Global Setup
+const machine = new VendingMachine();
 
 describe("vending machine", () => {
   it("should accept valid coins", () => {
     // Setup
-    const machine = new VendingMachine();
 
     // Exercise
     machine.insertCoin(500);
@@ -17,5 +20,39 @@ describe("vending machine", () => {
       500: 1,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
+  });
+
+  it("should have an insertCoin method", () => {
+    expect(machine.insertCoin).to.be.a("function");
+  });
+
+  it("should have a select row method", () => {
+    expect(machine.pressButton).to.be.a("function");
+  });
+
+  it("NO row selected, it should save the row and print to console when row is selected", () => {
+    machine.pressButton("A", 1);
+    expect(machine.row).to.be.equal("A");
+  });
+
+  it("NO row selected, it should display an error for non valid selection", () => {
+    machine.pressButton("E", 1);
+    expect(machine.pressButton).to.be("invalid row letter");
+  });
+
+  it("NO column selected, it should save the column and print to console when column is selected", () => {
+    machine.pressButton("A", 1);
+    expect(machine.column).to.be.equal(1);
+  });
+
+  it("NO column selected, it should display an error for non valid selection", () => {
+    machine.pressButton("A", 5);
+    expect(machine.pressButton).to.be("invalid column letter");
+  });
+});
+
+describe("Balance", () => {
+  it("if it's zero, it should rise when coin is inserted", () => {
+    machine.balance === 0;
   });
 });
